fb9187c69a1647016ebb9eba86e3ceb44b83347c
MalcarAle <alexandre.malcar@gmail.com>
2023-07-14 09:31:23 -0300

    adicionado rotas, falta corrigir a HOME

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import GlobalStyles from "./styles/global"
 import { ThemeProvider } from "styled-components"
 
 import theme from "./styles/theme"
-import { Home } from "./pages/Home"
+
+import { Routes } from "./routes"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
